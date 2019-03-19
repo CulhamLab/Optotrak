9cbb5ba12183166662fe4716d849074586d7e135
kmstubbs <kmstubbs@gmail.com>
2019-03-19 00:19:22 -0400

    closes audio + saves global struct

--- AddToPath/OptotrakComplete.m
+++ AddToPath/OptotrakComplete.m
@@ -1,2 +1,33 @@
-function OptotrakComplete
-%TODO
+%[filepath, opto] = OptotrakComplete
+%
+%An optional script to close the audio player if it is still open and save
+%the opto struct to mat file. Saves to the dat file directory with a name
+%containing the dat fileset name and a timestamp.
+%
+%Also returns the path to the saved file and a copy of the opto struct.
+%
+%When a trigger prep is given a trial number, it will have stored:
+% 1. expected filename of dat file
+% 2. time of trigger start
+% 3. whether data passed checks or not
+% 4. the data structure
+function [filepath, opto] = OptotrakComplete
+global opto
+
+%% Close audio player
+if isfield(opto, 'sound_handle')
+    try
+        PsychPortAudio('Close', opto.sound_handle);
+    end
+end
+
+%% Save global struct
+filepath = [opto.DIRECTORY_DATA  opto.FILENAME_SAVE];
+fprintf('Writing opto struct to: %s\n', filepath);
+if exist(filepath, 'file')
+    OptotrakWarning('The mat file already exists so the complete script has already been run. The prior file will be overwritten.');
+end
+save(filepath, 'opto')
+
+%% Done
+disp Completed!
