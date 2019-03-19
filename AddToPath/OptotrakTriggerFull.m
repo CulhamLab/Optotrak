017346a4a339a795bcb31fabe8fe676a62a25dec
kmstubbs <kmstubbs@gmail.com>
2019-03-19 00:18:26 -0400

    trigger shortcut

--- AddToPath/OptotrakTriggerFull.m
+++ AddToPath/OptotrakTriggerFull.m
@@ -1,7 +1,22 @@
-%A complete Optotrak trigger
-%Will hault script until trigger is complete (see OptotrakTriggerStart and OptotrakTriggerStop scripts for more detail)
-%If another action must immediately follow the start of the trigger, call OptotrakTriggerStart, perform those actions, and then call OptotrakTriggerStop
-%For very time-sensitive actions, you might need to split up OptotrakTriggerStart
+%[time_started] = OptotrakTriggerFull
+%
+%Must call OptotrakPrepareTrigger before this.
+%
+%A trigger consists of:
+% OptotrakPrepareTrigger (slow, call this well before the trial begins)
+% OptotrakTriggerStart (fast, sets pin high to trigger OTCollect to start
+%                       recording immediately)
+% OptotrakTriggerStop (fast unless Start was called too recently because
+%                      OTCollect needs some time to detect the signal,
+%                      should be called before recording ends to prevent a 
+%                      potential false trigger, sets pin low)
+%
+%This script combines the trigger start and stop meaning that it **WILL WAIT**
+%for the duration of opto.TIMING.MINIMUM_BETWEEN_HIGH_LOW_MSEC
+%
+%If there is something time-sensitive that must immediately follow the
+%trigger start, call OptotrakTriggerStart instead and then call
+%OptotrakTriggerStop later (before recording finishes).
 function [time_started] = OptotrakTriggerFull
 time_started = OptotrakTriggerStart;
 OptotrakTriggerStop;
