117eb8cc93b6f4e723883157c52d820c4e60763d
kmstubbs <kmstubbs@gmail.com>
2019-03-20 14:13:47 -0400

    Create Optotrak_Experiment_Template.m

---
+++ Collection/Optotrak_Experiment_Template.m
@@ -0,0 +1,14 @@
+%Optotrak_Experiment_Template(participant_number, run_number, override_start_trial_number)
+%
+%TODO: description
+function Optotrak_Experiment_Template(participant_number, run_number, override_start_trial_number)
+
+%% Check inputs
+
+
+%% Debug mode (disables hardware)
+p.DEBUG = true;
+
+%% Parameters (make changes here)
+
+
